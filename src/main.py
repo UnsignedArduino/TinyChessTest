38d5a164f1e9d6ae17fdeca208df1b3a8c1d098b
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-09-30 21:10:27 -0400

    Can fetch source code of TinyChess

--- src/main.py
+++ src/main.py
@@ -1,4 +1,52 @@
-from utils.logger import create_logger
 import logging
+from argparse import ArgumentParser
+
+from tinychesstest import fetch_source_code
+from utils.logger import create_logger
 
 logger = create_logger(name=__name__, level=logging.DEBUG)
+
+parser = ArgumentParser(
+    prog="TinyChessTest",
+    description="A program for tournament setup for elo testing the TinyChess engine.",
+)
+parser.add_argument(
+    "--engine-1-commit",
+    "--e1c",
+    metavar="COMMIT",
+    type=str,
+    required=True,
+    help="TinyChess commit for engine 1. Is passed to `git checkout`, so it can be a "
+    "commit hash or branch.",
+)
+parser.add_argument(
+    "--engine-2-commit",
+    "--e2c",
+    metavar="COMMIT",
+    type=str,
+    required=True,
+    help="TinyChess commit for engine 2. Is passed to `git checkout`, so it can be a "
+    "commit hash or branch.",
+)
+
+args = parser.parse_args()
+logger.debug(f"Arguments received: {args}")
+
+engine1: str = args.engine_1_commit.lower().strip()
+engine2: str = args.engine_2_commit.lower().strip()
+
+if engine1 == engine2:
+    logger.warning(
+        f"Engine 1 {engine1} and engine 2 {engine2} are the same, no elo differences "
+        f"expected!"
+    )
+
+engine1_dir, engine2_dir = fetch_source_code(engine1, engine2)
+# For testing
+# engine1_dir = Path(
+#     r"E:\TinyChessTest\working\sources\1870905aca990956a53c5cbe8dfa6c2d786ea57e\TinyChess"
+# )
+# engine2_dir = Path(r"E:\TinyChessTest\working\sources\main\TinyChess")
+
+logger.debug(f"Engine 1 directory is at {engine1_dir}")
+logger.debug(f"Engine 2 directory is at {engine2_dir}")
---
+++ src/tinychesstest.py
@@ -0,0 +1,87 @@
+import logging
+import stat
+from pathlib import Path
+from shutil import rmtree
+from subprocess import run
+from typing import Optional
+
+from utils.logger import create_logger
+
+logger = create_logger(name=__name__, level=logging.DEBUG)
+WORKING_SPACE_DIR = Path.cwd() / "working"
+SOURCE_CODE_DIR = WORKING_SPACE_DIR / "sources"
+
+CLONE_URL = "https://github.com/Bobingstern/TinyChess"
+
+
+def run_cmd(cmd: str, cwd: Optional[Path] = None):
+    """
+    Run a shell command.
+
+    :param cmd: Command to run.
+    :param cwd: Current working directory to set.
+    """
+    logger.debug(
+        f"Running command {cmd}" + f" with cwd {cwd}" if cwd is not None else ""
+    )
+    run(cmd, shell=True, cwd=cwd)
+
+
+def delete_dot_git_dir(path: Path):
+    """
+    Deletes a .git directory.
+
+    :param path: Path to the .git directory.
+    """
+    logger.debug(f"Deleting git directory {path}")
+    for p in path.rglob("*"):
+        if p.is_file():
+            logger.debug(f"Changing file permission of {p} to S_IWRITE")
+            p.chmod(stat.S_IWRITE)
+    logger.debug(f"Deleting directory {path}")
+    rmtree(path)
+
+
+def fetch_source_code(engine1: str, engine2: str) -> tuple[Path, Path]:
+    """
+    Fetch the source code of both engine commits.
+
+    :param engine1: Hash or branch or "cwd"
+    :param engine2: Hash or branch or "cwd"
+    :return: A tuple of two pathlib.Path pointing to the source code directories.
+    """
+    logger.info(f"Fetching source code for engine 1 ({engine1}) and 2 ({engine2})")
+    SOURCE_CODE_DIR.mkdir(parents=True, exist_ok=True)
+    engine1_source_dir = SOURCE_CODE_DIR / engine1
+    engine2_source_dir = SOURCE_CODE_DIR / engine2
+    logger.debug(f"Engine 1 source directory is {engine1_source_dir}")
+    logger.debug(f"Engine 2 source directory is {engine2_source_dir}")
+
+    for dir_to_create in (engine1_source_dir, engine2_source_dir):
+        if dir_to_create.exists():
+            dot_git_dir = dir_to_create / "TinyChess" / ".git"
+            if dot_git_dir.exists():
+                delete_dot_git_dir(dot_git_dir)
+            logger.debug(f"Removing {dir_to_create}")
+            rmtree(dir_to_create)
+        logger.debug(f"Creating {dir_to_create}")
+        dir_to_create.mkdir()
+
+    run_cmd(f"git clone {CLONE_URL}", engine1_source_dir)
+    engine1_source_dir = engine1_source_dir / "TinyChess"
+    run_cmd(f"git checkout {engine1}", engine1_source_dir)
+
+    run_cmd(f"git clone {CLONE_URL}", engine2_source_dir)
+    engine2_source_dir = engine2_source_dir / "TinyChess"
+    run_cmd(f"git checkout {engine2}", engine2_source_dir)
+
+    return engine1_source_dir, engine2_source_dir
+
+
+def cmake_compile(path: Path):
+    """
+    Compile a CMake project.
+
+    :param path: The path to the directory with the CMakeLists.txt file.
+    """
+    logger.info("")
